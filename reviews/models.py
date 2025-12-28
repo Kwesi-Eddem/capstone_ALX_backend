7742567f01dbaddeeecb7bfa1011063e40eafd26
addem <kwesi.eddem@gmail.com>
2025-12-28 17:19:30 +0000

    review class created

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+class Review(models.Model):
+    movie_title = models.CharField(max_length=255)
+    review_content = models.TextField()
+    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
