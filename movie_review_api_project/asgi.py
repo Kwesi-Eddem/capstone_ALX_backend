e6e8d9758a39d3595f30ca0acc5fc7c627faeeec
addem <kwesi.eddem@gmail.com>
2025-12-28 08:06:57 +0000

    overhaul commit

--- movie_review_api_project/asgi.py
+++ movie_review_api_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for alx_capstone_project project.
+ASGI config for movie_review_api_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_capstone_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_review_api_project.settings')
 
 application = get_asgi_application()
--- movie_review_api_project/wsgi.py
+++ movie_review_api_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for alx_capstone_project project.
+WSGI config for movie_review_api_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_capstone_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_review_api_project.settings')
 
 application = get_wsgi_application()
