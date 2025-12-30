d70f8eb628bcfe74b84ed782690f3e86f56ef74a
addem <kwesi.eddem@gmail.com>
2025-12-30 20:54:27 +0000

    reviewadmin added

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Review
 # Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('movie_title','rating','user','created_at')
+    search_fields = ('movie_title',"review_content")
+    list_filter = ('rating','created-at')
